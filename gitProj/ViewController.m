5b1bfbe4eccc4e78589aac772b547046b5ebae59
Raghu MJ <raghumj@Raghus-MacBook-Air.local>
2019-01-07 19:51:36 +0530

    updated viewcontroller

--- gitProj/ViewController.m
+++ gitProj/ViewController.m
@@ -18,7 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

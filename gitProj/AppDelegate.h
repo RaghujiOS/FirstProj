7ab8299744203a06b126f557834492509cfb6025
Raghu MJ <raghumj@Raghus-MacBook-Air.local>
2019-01-07 19:49:00 +0530

    first commit

---
+++ gitProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitProj
+//
+//  Created by Raghu MJ on 07/01/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitProj
+//
+//  Created by Raghu MJ on 07/01/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
